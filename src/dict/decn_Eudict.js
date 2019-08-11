cc8b1e4b9b6fd5ad5aa0d6a49e436c3378e3d67a
Fei <wu.qunfei@gmail.com>
2019-08-11 12:39:31 +0200

    adding parser logic

--- src/dict/decn_Eudict.js
+++ src/dict/decn_Eudict.js
@@ -23,13 +23,61 @@ class Decn_Eudict {
         if (!word) {
             return null;
         }
-        let base = 'http://www.godic.net/dicts/de/';
-        let url = base + encodeURIComponent(word);
+        let results = await Promise.all([this.queryWord(word)]);
+        return [].concat(...results).filter(x => x);
+    }
 
-        let terms = JSON.parse(await api.fetch(url));
-        if (terms.length == 0) {
-            return null;
+    async queryWord(word) {
+        function T(node) {
+            if (!node)
+                return '';
+            else
+                return node.innerText.trim();
         }
 
+        let base = 'http://www.godic.net/dicts/de/';
+        let url = base + encodeURIComponent(word);
+        let notes = [];
+        let doc = "";
+        let parser = new DOMParser();
+        try {
+            let response = api.fetch(url)
+            doc = parser.parseFromString(response, 'text/html');
+        } catch (error) {
+            console.log("Request Error: %s", error)
+            return notes;
+        }
+
+        let headSection = doc.querySelector('#dict-body>#exp-head');
+        if (!headSection) return null;
+        let expression = T(headSection.querySelector('.word'));
+        if (!expression) return null;
+        let reading = T(headSection.querySelector('.Phonitic'));
+        let css = this.renderCSS();
+
+        notes.push({
+            css,
+            expression,
+            reading
+        });
+        return notes;
     }
+
+    renderCSS() {
+        let css = `
+            <style>
+            span.eg,
+            span.exp,
+            span.cara
+            {display:block;}
+            .cara {color: #1C6FB8;font-weight: bold;}
+            .eg {color: #238E68;}
+            #phrase I {color: #009933;font-weight: bold;}
+            span.cet  {margin: 0 3px;padding: 0 3px;font-weight: normal;font-size: 0.8em;color: white;background-color: #5cb85c;border-radius: 3px;}
+            </style>`;
+
+        return css;
+    }
+
+
 }
